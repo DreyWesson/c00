7f089f2a1213eded485954f585692374a97533ed
dreywesson <dreywesson@gmail.com>
2022-07-24 10:33:58 +0100

    c00

---
+++ ex05/ft_print_comb.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/24 09:06:24 by doduwole          #+#    #+#             */
+/*   Updated: 2022/07/24 10:31:45 by doduwole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_print_char(int a, int b, int c)
+{
+	int	condition;
+
+	if (!(a == b || a == c || b == c))
+	{
+		write(1, &a, 1);
+		write(1, &b, 1);
+		write(1, &c, 1);
+		if (a == '7' && b == '8' && c == '9')
+			condition = 1;
+		else
+		{
+			write(1, ", ", 1);
+			condition = 0;
+		}
+	}
+	return (condition);
+}
+
+void	ft_print_comb(void)
+{
+	int	i;
+	int	j;
+	int	k;
+	int	condition;
+
+	i = '0';
+	while (i <= '7')
+	{
+		j = i + 1;
+		while (j <= '8')
+		{
+			k = j + 1;
+			while (k <= '9')
+			{
+				condition = ft_print_char(i, j, k);
+				if (condition == 1)
+					break;
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
